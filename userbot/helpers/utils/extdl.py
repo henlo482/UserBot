b85567c8d095db1463fb78e732757c59580dc427
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-02 14:55:38 +0530

    Update extdl.py

--- userbot/helpers/utils/extdl.py
+++ userbot/helpers/utils/extdl.py
@@ -4,4 +4,4 @@ from subprocess import PIPE, Popen
 def install_pip(pipfile):
     process = Popen(["pip", "install", f"{pipfile}"], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
-    return stdout
+    return stdou
